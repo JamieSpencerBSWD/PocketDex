1fc635944458b9f1136c8309d25f875b6322d0e7
DakotaSpencer <dakota.spencer0327@gmail.com>
2025-10-25 22:46:35 -0500

    maybe now it uses/pocketdex

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
     return (
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter basename="/PocketDex">
                 <NavBarx loggedInEmail={loggedInEmail} onLogout={handleLogout} />
                 <div className="pages">
                     <Routes>
